25d3bebb7d5f730710490eac52de289e7de60303
KemalMaulana24 <aryakemal242@gmail.com>
2024-10-21 21:13:04 +0800

    Add files via upload

---
+++ PROGRAM_9.py
@@ -0,0 +1,10 @@
+# Fungsi untuk menjumlahkan dua angka
+def penjumlahan(a, b):
+    return a + b
+
+# Contoh penggunaan
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+hasil = penjumlahan(angka1, angka2)
+print("Hasil penjumlahan:", hasil)
